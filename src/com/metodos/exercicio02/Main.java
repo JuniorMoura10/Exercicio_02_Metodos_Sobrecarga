b138abf1a849907236bef3c41d2d6717cc8e94fc
Júnior Moura <geraldomartins10@gmail.com>
2022-06-08 12:03:26 -0300

    exercício sobrecarga

---
+++ src/com/metodos/exercicio02/Main.java
@@ -0,0 +1,11 @@
+package com.metodos.exercicio02;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Quadrilatero.area(2);
+        Quadrilatero.area(2, 3);
+        Quadrilatero.area(2, 3, 10);
+    }
+}
